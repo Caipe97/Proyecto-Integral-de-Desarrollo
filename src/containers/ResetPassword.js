95b979e04d0b2ba7e90450e1d5da23dc025a77eb
andreecueva97 <60671503+andreecueva97@users.noreply.github.com>
2021-09-06 21:02:41 -0300

    update ResetPasswordJS

--- src/containers/ResetPassword.js
+++ src/containers/ResetPassword.js
@@ -4,6 +4,9 @@ import { resetPassword } from '../actions';
 import { useHistory } from "react-router-dom";
 import FormInput from '../components/FormInput';
 import './App.css';
+import logo2 from '../images/phone.png';
+import logo from '../images/logo_small.png';
+
 
 const mapStateToProps = (state) => {
   return {
@@ -40,15 +43,35 @@ function ResetPassword(props) {
   };
   
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
+   <div className="contenedorReset">
+ <header className="headerReset">
+      <div className="box" style={{}}>
+          <img src={logo} alt="imagen tracking" style={{width: 100}} />
+      </div>
+      </header>
+
+   <div className="contenidoReset" style={{background:"#fff",  textAlign: 'center',  alignItems: 'center',  justifyContent: 'center',  }}>
       <h1 className='f1'>ResetPassword</h1>
       <form>
         <FormInput name='password' type='password' placeholder='New password' handleChange={handleChange} required/>
       </form>
       <button onClick={handleSubmit}>ResetPassword</button>
-      <p style={{color: 'white'}}>{message}</p>
+      <p style={{color: 'black'}}>{message}</p>
       <button type="button" onClick={() => history.push("/login")}>Go to Login</button>
     </div>
+    <footer className="footerReset">
+    <div style={{width:'100%',position:'relative'}}> 
+      <div className="box" style={{width:'50%'}}>
+      <h3>Copyright 2021 JMA Group Ltd.</h3>
+          </div>
+          <div className="box" style={{  position:'absolute',right:'50px'}}>
+          <img src={logo2} alt="imagen phone" style={{width: 40,height:40}} />
+          <h3 style={{marginTop:'-5px'}}>4568-9430</h3>
+      </div>
+      </div>
+    </footer>
+    </div>
+
   );
 }
 
