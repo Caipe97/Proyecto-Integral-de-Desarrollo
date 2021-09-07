2ee32625cb38a53f53954d7d5c2464d641db14b6
andreecueva97 <60671503+andreecueva97@users.noreply.github.com>
2021-09-06 21:00:15 -0300

    update registerJS css and structure

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -4,6 +4,9 @@ import { register } from '../actions';
 import { useHistory } from "react-router-dom";
 import FormInput from '../components/FormInput';
 import './App.css';
+import logo2 from '../images/phone.png';
+import logo from '../images/logo_small.png';
+import logo1 from '../images/avatar.png';
 
 const mapStateToProps = (state) => {
   return {
@@ -47,8 +50,21 @@ function Register(props) {
   };
 
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
-      <h1 className='f1'>Register</h1>
+    <div className="contenedorReset">
+<header className="headerReset">
+     <div className="box" style={{}}>
+         <img src={logo} alt="imagen tracking" style={{width: 100}} />
+     </div>
+     </header>
+
+     <div className='contenidoReset' style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
+     <div className="box" style={{}}>
+         <img src={logo1} alt="imagen tracking" style={{width: 100}} />
+     </div>
+     <div style={{marginTop:45}}> 
+     <h1 className='f1'>Register</h1>
+     </div>
+    
       <form style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100%', flexDirection: 'column'}}>
         <FormInput name='name' type='name' placeholder='name' handleChange={handleChange} required/>
         <FormInput name='surname' type='surname' placeholder='surname' handleChange={handleChange} required/>
@@ -59,6 +75,19 @@ function Register(props) {
       <button type="button" onClick={() => history.push("/")}>Go to Home</button>
       <button type="button" onClick={() => history.push("/login")}>Go to Login</button>
     </div>
+   <footer className="footerReset">
+   <div style={{width:'100%',position:'relative'}}> 
+      <div className="box" style={{width:'50%'}}>
+      <h3>Copyright 2021 JMA Group Ltd.</h3>
+          </div>
+          <div className="box" style={{  position:'absolute',right:'50px'}}>
+          <img src={logo2} alt="imagen phone" style={{width: 40,height:40}} />
+          <h3 style={{marginTop:'-5px'}}>4568-9430</h3>
+      </div>
+      </div>
+   </footer>
+   </div>
+
   );
 }
 
