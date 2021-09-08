eab7d149b6b12736a6c935e60de8493f3dfca5e3
Julian Livrone <julian.livrone@gmail.com>
2021-09-08 10:08:11 -0300

    prueba fallida aproposito x2

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -15,7 +15,6 @@ function App() {
           <Route exact path="/" component={Login}/>
           <Route exact path="/resetPassword" component={ResetPassword} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/profile" component={Profile}/>
         </Switch>
       </div>
     );
