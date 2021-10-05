639a7cf02135edd39b7206ea54522158f15f5517
Julian Livrone <julian.livrone@gmail.com>
2021-10-05 12:28:09 -0300

    Tests rompiendo en progileMainPage test solucionado

--- src/components/ProfileMainPage/ProfileMainPage.test.js
+++ src/components/ProfileMainPage/ProfileMainPage.test.js
@@ -9,28 +9,6 @@ describe('ProfileMainPage tests', () => {
 
     beforeEach(() => {
         let mockProps2 = {
-            
-                userId: 1,
-                    name: 'julian',
-                    surname: 'livrone',
-                    email: 'julianlivrone@gmail.com',
-                    password: 'asd',
-                    gender: 'masculino',
-                    birthday: '2010-01-21',
-                    weight: 90,
-                    height: 170,
-                    meals:[
-                        {mealId: 21, name: 'queso', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1},
-                        {mealId: 22, name: 'carne', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1},
-                        {mealId: 23, name: 'jamon', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1}
-                    ],
-                    onGetMealsFromUser:jest.fn(),
-            // onResetPassword: jest.fn(),
-            onLogout: jest.fn(),
-            preventDefault:  jest.fn()
-          };
-          let mockProps = {
-            
             userId: 1,
                 name: 'julian',
                 surname: 'livrone',
@@ -40,10 +18,34 @@ describe('ProfileMainPage tests', () => {
                 birthday: '2010-01-21',
                 weight: 90,
                 height: 170,
-                meals:[],
+                meals:[
+                    {mealId: 21, name: 'queso', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1},
+                    {mealId: 22, name: 'carne', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1},
+                    {mealId: 23, name: 'jamon', foodsAndQuantity: {quantity: 1, foods: {foodId: 1, name: 'Milanesa', recommendedServing: 85, caloriesPerServing: 198, createdAt: '2021-09-15T19:58:04.486Z'}}, dateEaten: '2021-09-09T00:00:00.000Z', userId: 1}
+                ],
+                foodCategories: [],
                 onGetMealsFromUser:jest.fn(),
-        onLogout: jest.fn(),
-        preventDefault:  jest.fn()
+                onGetFoodCategories: jest.fn(),
+                // onResetPassword: jest.fn(),
+                onLogout: jest.fn(),
+                preventDefault:  jest.fn()
+          };
+          let mockProps = {
+            userId: 1,
+            name: 'julian',
+            surname: 'livrone',
+            email: 'julianlivrone@gmail.com',
+            password: 'asd',
+            gender: 'masculino',
+            birthday: '2010-01-21',
+            weight: 90,
+            height: 170,
+            meals:[],
+            foodCategories: [],
+            onGetMealsFromUser:jest.fn(),
+            onGetFoodCategories: jest.fn(),
+            onLogout: jest.fn(),
+            preventDefault:  jest.fn()
       };
         wrapper = shallow(<ProfileMainPage {...mockProps} history={historyMock}/>);
         wrapper2 = shallow(<ProfileMainPage {...mockProps2} history={historyMock}/>);
